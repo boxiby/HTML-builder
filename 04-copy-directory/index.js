6958e1604f6cc2af7ecb00255004b8e251dbae98
Anton Korobkin <3597052@gmail.com>
2023-05-09 21:18:35 +0300

    feat: add a script for copying directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const { mkdir, readdir, copyFile } = require('fs/promises');
+const path = require('path');
+
+
+async function copyDir() {
+  try {
+    await mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true });
+
+    const files = await readdir(path.resolve(__dirname, 'files'));
+
+    for (const file of files) {
+      const filePath = path.join(__dirname, 'files', file);
+      const copyPath = path.join(__dirname, 'files-copy', file);
+
+      await copyFile(filePath, copyPath);
+    }
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+copyDir();
 
